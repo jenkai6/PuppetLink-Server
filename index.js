847f8e71ad90bc68a66010c1c224c2342433e2a6
JENKAI LIU <kenny0408237@gmail.com>
2024-03-11 12:46:38 +0800

    create HTTPS server

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+// 引入 HTTPS 模組
+const https = require('https');
+// 引入文件系統模組
+const fs = require('fs');
 // 引入 Express 框架
 const express = require('express');
 // 創建一個 Express 應用程式實例
 const app = express();
 // 指定伺服器監聽的 Port
 const port = 8080;
+
+// 設定關於伺服器的選項。以下提供證明文件，以確保安全的 https 通訊。
+var options = {
+  key: fs.readFileSync('./ssl/server-key.pem'),
+  ca: [fs.readFileSync('./ssl/cert.pem')],
+  cert: fs.readFileSync('./ssl/server-cert.pem')
+};
+
+// 建立 HTTPS 伺服器
+// 第一個引數:伺服器的設定；第二個引數: Callback function，當有 req 進來就執行。在此我們是用 express處理這些 req。
+const server = https.createServer(options, app);
 
 // 設置樣板引擎（Template Engines） 為 EJS 
 // 補充: Template Engine（或稱 View Engine） 可以透過 JS 動態產生 HTML
@@ -30,7 +45,7 @@ app.get('/api/data', (req, res) => {
     res.json(data);
 });
 
-// 啟動伺服器，並開始監聽指定的 Port
-app.listen(port, ()=>{
-    console.log(`PuppetLink app listening on port ${port}!`);
+// 啟動 HTTPS 伺服器，並開始監聽指定的 Port
+server.listen(port, () => {
+    console.log(`PuppetLink HTTPS server running on port ${port}!`);
 });
