0a84c0a2b74f221ba4f28c6781a05cdc69963d9f
JENKAI LIU <kenny0408237@gmail.com>
2024-03-12 15:46:48 +0800

    add /realtime page

--- index.js
+++ index.js
@@ -34,13 +34,16 @@ app.set('view engine', 'ejs');
 // 處理根路徑 '/' 的 GET 請求，並返回在 views 資料夾底下 'index.ejs' 的視圖（即 template + data 生成後的結果）
 app.get('/', (req, res) => {
     res.render('index');  // 不用 .ejs 副檔名
-    console.log(`/ get client req`);
 })
 
 // 處理 '/user' 路徑的 GET 請求，並返回在 views 資料夾底下 'user.ejs' 的視圖
 app.get('/user', (req, res) => {
     res.render('user');
-    console.log(`/user get client req`);
+})
+
+// 處理 '/realtime' 路徑的 GET 請求，並返回在 views 資料夾底下 'realtime.ejs' 的視圖
+app.get('/realtime', (req, res) => {
+  res.render('realtime');
 })
 
 // 處理 '/api/data' 路徑的 GET 請求，並返回 JSON 格式的資料
@@ -53,40 +56,65 @@ app.get('/api/data', (req, res) => {
 
     // 使用 res.json() 方法將 JS 物件轉換為 JSON 格式，並返回給客戶端
     res.json(sensor);
-    console.log(`/api/data get client req`);
 });
 
 /** WebSocket 處理雙向數據傳輸**/
 // 建立 WebSocket Server，並與 HTTP server 連結在一起。意味著兩者將共享相同的 Port
 const wss = new WebSocket.Server({ server });
-let wsClients = [];
+let allClients = [];
+let userClinets = [];
+let realtimeClinets = [];
 
 //當有 client 連線成功時
-wss.on('connection', ws => {
-    wsClients.push(ws);
-    console.log(`Client connected! Total ${wsClients.length}`)
-    
-    // 當收到client消息時
-    ws.on('message', data => {
-        // 只處理第一順位的 clinet
-        if(wsClients.indexOf(ws) !== 0)
-            return;
+wss.on('connection', (ws, req) => {
+    allClients.push(ws);
+    if(req.url === '/realtime') realtimeClinets.push(ws);
+    if(req.url === '/user') userClinets.push(ws);
+    console.log(`Client connected 'ALL'! Total ${allClients.length}`) 
+    console.log(`Client connected '/user'! Total ${userClinets.length}`) 
+    console.log(`Client connected '/realtime'! Total ${realtimeClinets.length}`) 
+ 
+    // 當收到 client 消息時
+    ws.on('message', data => {     
+      // 如果消息來自 /user 目錄
+      if(req.url === '/user'){
+        // 如果收到第一順位 clinet 的 sensor 資料
+        if(userClinets.indexOf(ws) === 0){
+          // 將資料 JSON to JS object，並更新 sensor 資料
+          sensor = JSON.parse(data);  
+          console.log(sensor);
 
-        // JSON 資料格式 to JS object
-        sensor = JSON.parse(data);  
-        // console.log(`from client${wsClients.indexOf(ws)}: ${sensor}`); // 可在 terminal 看收到的訊息
-        console.log(sensor);
+          /// 發傳給所有在 /realtime 目錄的 client 
+          realtimeClinets.forEach(client => {
+            client.send(JSON.stringify(sensor));
+          })
+        }
+
+        // 回傳給所有在 /user 目錄的 clinet 他們目前的排順
+        userClinets.forEach(client => {
+          client.send(userClinets.indexOf(client));
+          // console.log(userClinets.indexOf(client));
+        })
+      }
     })
 
     // 當連線關閉
     ws.on('close', () => {
-      let index = wsClients.indexOf(ws);
-      wsClients.splice(index, 1);
-      console.log(`Close connected! Total ${wsClients.length}`)
-      // 只處理第一順位的 clinet
-      if(wsClients.length === 0){
+      let index = allClients.indexOf(ws);
+      allClients.splice(index, 1);
+
+      index = userClinets.indexOf(ws);
+      if(req.url === '/user') userClinets.splice(index, 1);
+
+      index = realtimeClinets.indexOf(ws);
+      if(req.url === '/realtime') realtimeClinets.splice(index, 1);
+
+
+      console.log(`Close connected! Total ${allClients.length}`)
+
+      // 如果沒有使用者，則清空 sensor 數據
+      if(userClinets.length === 0)
         sensor = null;
-      }
     })
   })
 
